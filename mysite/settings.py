362b406f0e081c9ce27721f8c8bc5f9510c91aa1
WangTianyi <tywang918@gmail.com>
2015-12-03 15:15:19 -0500

    email user upon registration

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dukebooktrading@gmail.com'
+EMAIL_HOST_PASSWORD = 'dukecs316'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
