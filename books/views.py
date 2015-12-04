877212e0e2e86da72911349e2de5b247831e9ea5
WangTianyi <tywang918@gmail.com>
2015-12-04 11:03:01 -0500

    added email function for listing a listing

--- books/views.py
+++ books/views.py
@@ -46,9 +46,9 @@ def register(request):
 			from_email = settings.EMAIL_HOST_USER
 			to_email = [form_email]
 			contact_message = """Hi %s: 
-			Thank you for registering at dukebooktrading. Thank you.
+Thank you for registering at dukebooktrading. Thank you.
 
-			Duke Book Trading Team""" %(form_username)
+				Duke Book Trading Team""" %(form_username)
 			send_mail(subject, contact_message,from_email, to_email,fail_silently = False)
 
 
@@ -124,6 +124,20 @@ def list_submit(request):
 			listing.start_time = int(time.time())
 			listing.active = True
 			listing.save()
+
+			form_email = request.user.email
+			form_username = request.user.username
+			subject = 'Your listing has been posted.'
+			from_email = settings.EMAIL_HOST_USER
+			form_title = listForm.cleaned_data['title']
+			to_email = [form_email]
+			contact_message = """Hi %s: 
+Thank you for listing at dukebooktrading. Your listing of %s has been posted. Thank you.
+
+				Duke Book Trading Team""" %(form_username, form_title)
+			send_mail(subject, contact_message,from_email, to_email,fail_silently = False)
+
+
 			return HttpResponseRedirect(reverse('books.views.all_books'))
 	else:
 		listForm = ListBookForm()
